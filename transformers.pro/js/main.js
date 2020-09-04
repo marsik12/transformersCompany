32fcb751a48e2cd7275354a6d207b4cde55ca8f7
Sherzod <sheraleks869@gmail.com>
2020-09-04 20:40:33 +0500

    Xizmatlar seksiyasiga rasmlar qo'shildi, mobile menyu uchun hamburger tayyorlandi va boshqa o'zgarishlar qilindi

--- transformers.pro/js/main.js
+++ transformers.pro/js/main.js
@@ -16,4 +16,46 @@ testWebP(function (support) {
 	}
 });
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("slider__item");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
+
+function hamburger(x) {
+	x.classList.toggle("change");
+}
+
+function myFunction() {
+	var x = document.getElementsByClassName("navigation");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
+
 // @@include '../vendors/owl.carousel.min.js';
